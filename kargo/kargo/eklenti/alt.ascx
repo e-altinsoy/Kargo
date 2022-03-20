3e582d66bd48e2cee18d74f32ebfde928462b6d0
e-altinsoy <57418626+e-altinsoy@users.noreply.github.com>
2022-03-20 16:12:18 +0300

    Kargo

---
+++ kargo/kargo/eklenti/alt.ascx
@@ -0,0 +1,2 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="alt.ascx.cs" Inherits="kargo.eklenti.WebUserControl2" %>
+<%--<div class="govde"><div class="copyright">Copyright © 2021 YBS Kargo | Tasarım: R & R</div></div>--%>
---
+++ kargo/kargo/eklenti/ust.ascx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace kargo.eklenti
+{
+    public partial class WebUserControl1 : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Kargo_Otomasyonu;Integrated Security=True";
+            SqlConnection con = new SqlConnection(CS);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from tbl_sayfalar", con);
+            Repeater1.DataSource = cmd.ExecuteReader();
+            Repeater1.DataBind();
+            con.Close();
+        }
+    }
+}
